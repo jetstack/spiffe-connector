ae1043f8f556cd01241e9630afbf5e12caa7f56c
Jake Sanders <i@am.so-aweso.me>
2022-04-27 15:56:28 +0100

    Implement gRPC server API and test config generator
    Signed-off-by: Jake Sanders <i@am.so-aweso.me>

--- internal/pkg/cryptoutil/cmd/main.go
+++ internal/pkg/cryptoutil/cmd/main.go
@@ -1,23 +1,60 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
-	"github.com/jetstack/spiffe-connector/internal/pkg/cryptoutil"
 	"os"
+
+	"github.com/jetstack/spiffe-connector/internal/pkg/cryptoutil"
 )
 
-const testConfig = ``
+const testConfig = `---
+acls:
+- match_principal: "spiffe://foo/bar/baz"
+  credentials:
+  - provider: "google"
+    object_reference: "service-account@example.com"
+  - provider: "aws"
+    object_reference: "aws::arn:foo"
+spiffe:
+  svid_sources:
+    files:
+      trust_domain_ca: ./ca.pem
+      svid_cert: ./svid_cert.pem
+      svid_key: ./svid_key.pem
+`
 
 // Generate testing material for use locally.
 func main() {
-	certs, err := cryptoutil.SelfSignedServingCert()
+	if len(os.Args) < 2 {
+		exitOnErr(errors.New("usage: " + os.Args[0] + " 'spiffe://your.domain/your/id'"))
+	}
+	certs, err := cryptoutil.GenerateTestCerts(os.Args[1])
 	exitOnErr(err)
-
+	leafCert := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certs.Certificate[0],
+	})
+	caCert := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certs.Certificate[1],
+	})
+	x509Key, _ := x509.MarshalPKCS8PrivateKey(certs.PrivateKey)
+	key := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: x509Key,
+	})
+	exitOnErr(os.WriteFile("ca.pem", caCert, 0o600))
+	exitOnErr(os.WriteFile("svid_cert.pem", leafCert, 0o600))
+	exitOnErr(os.WriteFile("svid_key.pem", key, 0o666))
+	exitOnErr(os.WriteFile("test.yaml", []byte(testConfig), 0o666))
 }
 
 func exitOnErr(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Couldn't generate certs")
+		fmt.Fprintf(os.Stderr, "Couldn't generate certs: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -2,22 +2,39 @@ package server
 
 import (
 	"context"
-	"google.golang.org/grpc"
+	"errors"
+	"log"
 	"net"
 
 	"github.com/spiffe/go-spiffe/v2/spiffegrpc/grpccredentials"
 	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/jetstack/spiffe-connector/internal/pkg/config"
+	"github.com/jetstack/spiffe-connector/internal/pkg/server/proto"
 )
 
 type Server struct {
+	proto.UnimplementedSpiffeConnectorServer
+}
+
+func (s *Server) GetCredentials(ctx context.Context, empty *emptypb.Empty) (*proto.GetCredentialsResponse, error) {
+	resp := &proto.GetCredentialsResponse{}
+	// Get the connecting SPIFFE ID
+	clientSVID, hasSVID := grpccredentials.PeerIDFromContext(ctx)
+	if !hasSVID {
+		return resp, errors.New("no SVID provided")
+	}
+	log.Printf("Obtaining credentials for %s\n", clientSVID.String())
+
+	return resp, nil
 }
 
 func (s *Server) Start(ctx context.Context) {
 
 	server := grpc.NewServer(grpc.Creds(grpccredentials.MTLSServerCredentials(config.CurrentSource, config.CurrentSource, tlsconfig.AuthorizeAny())))
-
+	proto.RegisterSpiffeConnectorServer(server, s)
 	listener, err := net.Listen("tcp", "[::]:9090")
 	if err != nil {
 		panic("fail")
